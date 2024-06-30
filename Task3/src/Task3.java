a12dacd0671926b3c746c1e050c3af7fe3f08825
Anarakom <skuznekov201333@gmail.com>
2024-06-30 09:34:53 +0200

    Update Task3.java

--- Task3/src/Task3.java
+++ Task3/src/Task3.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 public class Task3 {
     public static void main(String[]agrs){
-        double C = 36.6;
-        double F = C*9/5+32;
+        double C ;
+        Scanner in =new Scanner(System.in);
+        System.out.print("C=");
+        C = in.nextDouble();
+        double F = ((C*9)/5)+32;
         System.out.println(F);
 
 
