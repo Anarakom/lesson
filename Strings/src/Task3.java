a7cf8828847a4c6b8ae2ebdc9c31e06bbcb4dc45
Anarakom <skuznekov201333@gmail.com>
2024-09-13 22:19:32 +0200

    lesson

--- Strings/src/Task3.java
+++ Strings/src/Task3.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
-
 public class Task3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner in =new Scanner(System.in);
         System.out.print("Введите строку: ");
-        String input = scanner.nextLine();
-        String formattedInput = input.toLowerCase();
-        int i = 0;
-        int j = formattedInput.length() - 1;
+        String input = in.nextLine();
+        String formattedInput = input.toLowerCase().replaceAll("\\s+", "");
         boolean isPalindrome = true;
+        for (int i = 0, j = formattedInput.length() - 1; i < j; i++, j--) {
+            if (formattedInput.charAt(i) != formattedInput.charAt(j)) {
+                isPalindrome = false;
+                break;
+            }
+        }
         if (isPalindrome) {
             System.out.println("Строка является палиндромом.");
         } else {
